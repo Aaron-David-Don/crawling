5de85a2a9291990028898aa28cb0df29ebd775a1
Aaron-David-Don <aarondaviddon12@gmail.com>
2024-06-07 16:26:22 +0530

    Add files via upload

---
+++ googscholar1++.py
@@ -0,0 +1,36 @@
+from playwright.sync_api import sync_playwright
+import sys
+
+def run(playwright):
+    browser = playwright.chromium.launch(headless=True) 
+    context = browser.new_context()  
+    page = context.new_page()  
+
+    
+    #base_url = "https://scholar.google.com/scholar?q=sleep+inertia&hl=en&as_sdt=0,5&start="
+    base_url = "https://scholar.google.com/scholar?q=coffee&hl=en&as_sdt=0,5&start="
+
+    for i in range(0, 1000, 10):  # 0 to 90
+        url = f"{base_url}{i}"  # Construct the URL for the current page
+        page.goto(url)  
+        
+        # Wait for the necessary elements to load
+        page.wait_for_selector(".gs_ri")
+        
+        # Extract the information
+        results = page.query_selector_all(".gs_ri")
+        for result in results:
+            title_element = result.query_selector("h3.gs_rt")
+            title = title_element.text_content().strip()
+            link_element = title_element.query_selector("a")
+            link = link_element.get_attribute("href") if link_element else "No link available"
+            print(f"Title: {title}\nLink: {link}\n")
+    
+    
+    browser.close()
+
+# Ensure the script prints to the console using UTF-8 encoding
+sys.stdout.reconfigure(encoding='utf-8')
+
+with sync_playwright() as playwright:
+    run(playwright)

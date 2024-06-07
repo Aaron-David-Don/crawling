5f49ee953336da9d1d27af6b5813907e342b71c5
Aaron-David-Don <aarondaviddon12@gmail.com>
2024-06-08 00:53:20 +0530

    Updated

--- cleaning_links-EAUPB.py
+++ cleaning_links-EAUPB.py
@@ -1,13 +1,15 @@
+'''This code is to clean the output received from 
+extract_all_urls-playwright-bfs.py
+It provides only the links with no duplicates'''
 import re
 
 def extract_urls(input_file, output_file):
     # Regular expression for matching URLs
     url_pattern = re.compile(r'(https?://[^\s]+)')
     
-    # Set to store unique URLs
+    
     urls = set()
     
-    # Read the input file and extract URLs
     with open(input_file, 'r') as file:
         for line in file:
             matches = url_pattern.findall(line)
@@ -16,12 +18,12 @@ def extract_urls(input_file, output_file):
                 if not (match.endswith(',') or match.endswith(':')):
                     urls.add(match)
     
-    # Write the unique URLs to the output file
+    
     with open(output_file, 'w') as file:
         for url in sorted(urls):
             file.write(url + '\n')
 
-# Example usage
+
 input_file = 'output\page_1.md'
 output_file = 'output\outputpg1.txt'
 extract_urls(input_file, output_file)

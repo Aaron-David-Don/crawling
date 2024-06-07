ff8ff5ee2f8c6507f48d10233240e27b295e8c3a
Aaron-David-Don <aarondaviddon12@gmail.com>
2024-06-08 00:58:25 +0530

    Updated

--- firecrawl_scrape_pages.py
+++ firecrawl_scrape_pages.py
@@ -1,22 +1,20 @@
+'''This code is to scrape given pages of a website using firecrawl api '''
 import os
 from firecrawl import FirecrawlApp
 
-# Define the URLs to crawl
 urls = [
     "https://pubmed.ncbi.nlm.nih.gov/?term=sleep%20inertia&size=200&page=1",
     "https://pubmed.ncbi.nlm.nih.gov/?term=sleep%20inertia&size=200&page=2"
 ]
 
-# Initialize FirecrawlApp with the API key
-firecrawl = FirecrawlApp(
-    api_key="fc-33b4ec3c4b5a4e7fa5b40a66d95340cd",
-)
 
-# Create the output directory if it doesn't exist
+firecrawl = FirecrawlApp(
+    api_key="haha use ur api key",
+)
+#create one file
 output_dir = "output"
 os.makedirs(output_dir, exist_ok=True)
 
-# Iterate over each URL and perform the crawl
 for i, url in enumerate(urls, start=1):
     page_content = firecrawl.scrape_url(
         url=url,
@@ -30,7 +28,6 @@ for i, url in enumerate(urls, start=1):
         }
     )
 
-    # Save the result as a markdown file
     if 'markdown' in page_content:
         markdown_content = page_content['markdown']
         file_path = os.path.join(output_dir, f"page_{i}.md")
